38702fd101ab21e8899b4cdaa92d026b4cedccb2
amreenpatel123m-collab <amreenpatel123m@gmail.com>
2026-02-07 01:39:02 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 const API_KEY = "7cf8535c2aa2c745040de291475c23d2";
+const container = document.getElementById("movies");
 
+// List of known public domain movies (safe, legal)
 const movies = [
   { title: "Night of the Living Dead", id: "night_of_the_living_dead" },
   { title: "Plan 9 from Outer Space", id: "plan_9_from_outer_space" },
-  { title: "The Last Man on Earth", id: "last_man_on_earth_1964" },
   { title: "His Girl Friday", id: "his_girl_friday" },
   { title: "Charade", id: "charade_1963" },
   { title: "Sherlock Holmes", id: "sherlock_holmes_1939" }
 ];
 
-const container = document.getElementById("movies");
-
+// Loop and show
 movies.forEach(movie => {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(movie.title)}`;
 
